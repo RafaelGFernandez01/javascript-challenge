99cae6c04e4094b70ae6e2eeb3a115615216fac3
Rafael Fernandez <rafa7860@yahoo.com>
2021-03-22 20:40:42 -0400

    wip adding rows to tables

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,13 @@
 // from data.js
 var tableData = data;
 
+var table = document.getElementById('ufo-data-table');
+
+console.log('tableData ->', tableData);
+console.log('table ->', table);
+
+for (let index = 0; index < tableData.length; index++) {
+    const element = tableData[index];
+    console.log('elem ->', element);
+}
 // YOUR CODE HERE!
